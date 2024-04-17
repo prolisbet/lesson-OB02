c555261df6984c3c49c0fdd5b290ed888d06dcc5
prolisbet <lizatrushina96@gmail.com>
2024-04-17 04:12:04 +0300

    test code and correct it

--- main.py
+++ main.py
@@ -46,11 +46,51 @@ class Admin(User):
 
     def add_user(self, user):
         user_list.append(user)
-        print(f'Администратор {self} добавил в список пользователей {user}.')
+        print(f'Администратор добавил в список пользователей: {user.get_name()}.')
 
     def remove_user(self, user):
         user_list.remove(user)
-        print(f'Администратор {self} удалил из списка пользователей {user}.')
+        print(f'Администратор удалил из списка пользователей: {user.get_name()}.')
 
-        
 
+def show_user_list(list):
+    for i, user in enumerate(list, start=1):
+        print(f'{i}. {user.get_name()}')
+
+
+admin1 = Admin('8976341', 'Иванов Дмитрий Евгеньевич')
+user1 = User('5489230', 'Павлова Софья Сергеевна')
+user2 = User('4312895', 'Афанасьев Иван Георгиевич')
+user3 = User('1245783', 'Новикова Наталья Олеговна')
+user4 = User('9034187', 'Лопаткин Даниил Игоревич')
+
+admin1.add_user(user1)
+admin1.add_user(user2)
+admin1.add_user(user3)
+admin1.add_user(user4)
+
+show_user_list(user_list)
+
+admin1.remove_user(user2)
+
+show_user_list(user_list)
+
+
+try:
+    user1.__id
+except AttributeError:
+    print(None)
+
+try:
+    user1.__name
+except AttributeError:
+    print(None)
+
+try:
+    user1.__access_level
+except AttributeError:
+    print(None)
+
+print(f'Уникальный идентификатор (ID) - {user1.get_id()}')
+print(f'Имя пользователя - {user1.get_name()}')
+print(f'Уровень доступа - {user1.get_access()}')
